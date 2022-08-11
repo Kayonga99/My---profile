173bda3855866d1b899d2523246acd3e23c89f99
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-11 19:38:08 +0200

    Update branch.

--- js/main.js
+++ js/main.js
@@ -214,10 +214,131 @@ $(document).scroll((event) => {
   }
 });
 
+const modalData = [
+   {   id: 1,
+    heading1: 'NAC',
+    heading2: ['CANOPY', 'Back End Dev', '2022'],
+    description: 'New apostolic church pentocst devine service 2022 Capstone project.',
+    featuredImage: './photos/newp/first-bg.jpg',
+    languages: ['html', 'css', 'javascript'],
+    linkLive: 'https://kayonga99.github.io/Capstone',
+    linkSource: 'https://github.com/Kayonga99/Capstone',
+  },
+  {   id: 1,
+    heading1: 'NAC',
+    heading2: ['CANOPY', 'Back End Dev', '2022'],
+    description: 'New apostolic church pentocst devine service 2022 Capstone project.',
+    featuredImage: './photos/newp/first-bg.jpg',
+    languages: ['html', 'css', 'javascript'],
+    linkLive: 'https://kayonga99.github.io/Capstone',
+    linkSource: 'https://github.com/Kayonga99/Capstone',
+  },
+  {   id: 1,
+    heading1: 'NAC',
+    heading2: ['CANOPY', 'Back End Dev', '2022'],
+    description: 'New apostolic church pentocst devine service 2022 Capstone project.',
+    featuredImage: './photos/newp/first-bg.jpg',
+    languages: ['html', 'css', 'javascript'],
+    linkLive: 'https://kayonga99.github.io/Capstone',
+    linkSource: 'https://github.com/Kayonga99/Capstone',
+  },
+  {   id: 1,
+    heading1: 'NAC',
+    heading2: ['CANOPY', 'Back End Dev', '2022'],
+    description: 'New apostolic church pentocst devine service 2022 Capstone project.',
+    featuredImage: './photos/newp/first-bg.jpg',
+    languages: ['html', 'css', 'javascript'],
+    linkLive: 'https://kayonga99.github.io/Capstone',
+    linkSource: 'https://github.com/Kayonga99/Capstone',
+  },
+  {   id: 1,
+    heading1: 'NAC',
+    heading2: ['CANOPY', 'Back End Dev', '2022'],
+    description: 'New apostolic church pentocst devine service 2022 Capstone project.',
+    featuredImage: './photos/newp/first-bg.jpg',
+    languages: ['html', 'css', 'javascript'],
+    linkLive: 'https://kayonga99.github.io/Capstone',
+    linkSource: 'https://github.com/Kayonga99/Capstone',
+  },
+  {   id: 1,
+    heading1: 'NAC',
+    heading2: ['CANOPY', 'Back End Dev', '2022'],
+    description: 'New apostolic church pentocst devine service 2022 Capstone project.',
+    featuredImage: './photos/newp/first-bg.jpg',
+    languages: ['html', 'css', 'javascript'],
+    linkLive: 'https://kayonga99.github.io/Capstone',
+    linkSource: 'https://github.com/Kayonga99/Capstone',
+  },
+  {   id: 1,
+    heading1: 'NAC',
+    heading2: ['CANOPY', 'Back End Dev', '2022'],
+    description: 'New apostolic church pentocst devine service 2022 Capstone project.',
+    featuredImage: './photos/newp/first-bg.jpg',
+    languages: ['html', 'css', 'javascript'],
+    linkLive: 'https://kayonga99.github.io/Capstone',
+    linkSource: 'https://github.com/Kayonga99/Capstone',
+  },
+  {   id: 1,
+    heading1: 'NAC',
+    heading2: ['CANOPY', 'Back End Dev', '2022'],
+    description: 'New apostolic church pentocst devine service 2022 Capstone project.',
+    featuredImage: './photos/newp/first-bg.jpg',
+    languages: ['html', 'css', 'javascript'],
+    linkLive: 'https://kayonga99.github.io/Capstone',
+    linkSource: 'https://github.com/Kayonga99/Capstone',
+  },
+];
+
+function popData(card) {
+  const temp = document.createElement('template');
+  temp.innerHTML += `<section class="card-list-23">
+  <div class="desk-top">
+    <div class="">
+      <div class="x-class">
+        <h3 class="tonic-class-23">${card.heading1}</h3>
+        <h4 class="close-x">&times;</h4>
+      </div>
+        <div class="canopy">
+          <ul class="cards-ul">
+          ${card.heading2.map((heading, index) => `<li class="info${index}">${heading}</li>`).join('')}
+          </ul>
+        </div>
+        <img class="card" src="${card.featuredImage}" alt="work1Snapshot" />
+        <img class="nature-animated" src="${card.featuredImage2}" alt="nature animated picture" />
+        <div class="src-div">
+      <p class="src-description">
+        ${card.description2}
+      </p>
+      <div class="div-src">
+      <ul class="tools-src src-tools">
+        <li class="tools-li-src"> html</li>
+        <li class="tools-li-src">css</li>
+        <li class="tools-li">javaScript</li>
+        <li class="extra">github</li>
+        <li class="extra">ruby</li>
+        <li class="extra">Bootstraps  </li>
+      </ul>
+      <div class="view-project">
+        <a class="src-btn" href="${card.linkLive} ">See live <img src="./photos/icons/btn-icon.png" alt="button-icon"> </a>
+        <a class="src-btn" href="${card.linkSource}">See Source <img src="./photos/icons/blue-github.png" alt="github-icon"> </a>
+      </div>
+    </div>
+    </div>
+</section>
+`;
+  const box = document.getElementById('pop-up-modal');
+  box.appendChild(temp.content);
+}
+
 const popUp = document.querySelectorAll(".fa-eye")
 
+popUp.addEventListener("click", () => { 
+  
+})
 
-// local-storage
+
+
+local-storage
 const inputFields = document.forms['contact-form']
 const mail = inputFields.email;
 const messageInput = inputFields.message;
@@ -256,6 +377,7 @@ function validateEmail(email, event, errorMsg) {
     msg.style.fontSize = '15px';
   }
 }
+
 const form = document.getElementById('form');
 form.addEventListener('submit', (event) => {
   const errorMessage = 'Please enter an email address without any upper-case letters.';
