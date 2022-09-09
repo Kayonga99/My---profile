48e01e84a6658371241e4e3310e96a08ee975a50
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-09-10 00:23:30 +0200

    Update branch.

--- js/modal.js
+++ js/modal.js
@@ -123,44 +123,15 @@
 
 
 
-
-const openEls = document.querySelectorAll(".fa-eye");
-const closeEls = document.querySelectorAll("[data-close]");
-const isVisible = "is-visible";
-
-for (const el of openEls) {
-  el.addEventListener("click", function() {
-    const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisible);
-  });
-}
-
-for (const el of closeEls) {
-  el.addEventListener("click", function() {
-    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
-  });
-}
-
-document.addEventListener("click", e => {
-  if (e.target == document.querySelector(".modal.is-visible")) {
-    document.querySelector(".modal.is-visible").classList.remove(isVisible);
-  }
-});
-
-document.addEventListener("keyup", e => {
-  // if we press the ESC
-  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
-    document.querySelector(".modal.is-visible").classList.remove(isVisible);
-  }
-});
-
-
 const openModal = document.querySelectorAll(".fa-eye")
-
+const closeModal = document.querySelector(".close-x")
 const ourModal = document.querySelector(".openModal")
 
 openModal.forEach((e) => {
   e.addEventListener("click", () =>{
     ourModal.style.display = "block";
   })
+})
+closeModal.addEventListener("click", () =>{
+  openModal.style.display = "none";
 })
